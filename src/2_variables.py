8250595283953cd79400fd77a9b225bc478e6572
Grzegorz Wojdyga <g.wojdyga@gmail.com>
2022-01-31 08:45:04 +0100

    changes

--- src/2_variables.py
+++ src/2_variables.py
@@ -1,72 +1,105 @@
-def task():
+def task1():
     """
-    Function that can calculate square value of argument
-    Input: string with comma separated numbers
-    Output: two integers
+    Function that calculates sum of two argument values
+
+    Input: two integers
+    Output: one integers
     """
     pass
 
 
-def task():
+def task2():
     """
-    Function that calculates area of an equilateral triangle. Suare root of 3 is 1.732 return the area as string that
-    has two numbers after comma.
+    Function that calculates square value of argument
+
+    Input: one integer
+    Output: one integer
+    """
+    pass
+
+
+def task3():
+    """
+    Functions which accepts two numbers as string (separated by comma) and generates a string with the result of
+    their addition.
+
+    Input: n,m -> strings
+    Output: string
+    """
+
+
+def task4():
+    """
+    Functions which accepts a two numbers as int and generates a string with their concatination (e.g.  3,4 -> 34)
+
+    Input: two integers
+    Output: string
+    """
+
+
+def task5():
+    """
+    Function that calculates area of an equilateral triangle. Suare root of 3 is 1.732. It returns the area as string
+    that has two numbers after comma.
+
     Input: N -> float number
     Output: area of triangle as string such as: "1.23"
     """
     pass
 
 
-def task():
+def task6():
     """
-    Function that calculates area of an equilateral triangle. Square root of 3 is 1.732 return the area as string
+    Function that calculates area of an equilateral triangle. Square root of 3 is 1.732. It returns the area as string
     that has two numbers after comma.
+
     Input: a -> int number
     Output: area of triangle as string such as: "1.23"
     """
     pass
 
 
-def task():
+def task7():
     """
     Function that calculates an area of a right triangle with two sides - a and b (just like Pitagoras triangle).
     Return the area as string that has two numbers after comma.
+
     Input: a,b -> int numbers
     Output: area of triangle as string such as: "1.23"
     """
     pass
 
 
-def task():
+def task8():
     """
     Function that receives a sequence of space separated 4 single digits (e.g. '1 2 3 4') as input and then check
     whether they as one number (1234) is divisible by 5 or not.
+
     Input: string with 4 digits
     Output: Boolean value (True or False)
     """
     pass
 
 
-def task():
+def task9():
     """
     Function that computes the value of a+aa+aaa+aaaa with a given digit as the value of a
-    Input: digit
-    Output: number
+
+    Input: digit as integer
+    Output: number as integer
     """
     pass
 
 
-def task():
+def task10():
     """
     Function that take an integer number as string and print the "It is an even number" if the number is even,
     otherwise print "It is an odd number".
+
     Input: number
-    Output: number
+    Output: None
     """
     pass
-
-
-
 
 
 if __name__ == '__main__':
--- src/3_text_operation.py
+++ src/3_text_operation.py
@@ -1,21 +1,157 @@
-def task():
+def task1():
+    """
+    Function which receives a sequence of comma-separated numbers and generate a list which contains
+    every number
+
+    Input: string with comma separated numbers
+    Output: list with all the numbers
+    """
+    pass
+
+
+def task2():
+    """
+    Function that can accept two strings as input and print the string with maximum length in console. If two strings
+    have the same length, then the function should print all strings line by line.
+
+    Input: two strings
+    Output: one or two strings
+    """
+    pass
+
+
+def task3():
     """
     Function that take an object as input and prints its docs (__doc__) every sentence in one line.
+
     Input: name of object (e.g. int)
     Output: string ready to write.
     """
+    pass
 
 
-def task():
+def task4():
+    """
+    Function that can accept two strings as input and print all the letters that are in both strings.
+
+    Input: two strings
+    Output: None
+    """
+    pass
+
+
+def task5():
+    """
+    Function that transform given string to a string where all occurrences of its first char have been
+    changed to '$', except the first char itself (e.g. "asdfasdffdsa" -> "asdf$sdffds$")
+
+    Input: string
+    Output: string
+    """
+    pass
+
+
+def task6():
+    """
+    Given an email adress: username@companyname.com" format, function returns the user name
+    of a given email address. (e.g. "gregory@sda.pl" -> "gregory")
+    You may do it with or without regular expressions.
+
+    Input: string
+    Output: string
+    """
+    pass
+
+
+def task7():
+    """
+    Given many email adresses separated by comma, function returns the user names separated by comma
+    of a given emails address (e.g. "gregory@sda.pl,jan@google.com -> "gregory,jan")
+
+    Input: string
+    Output: string
+    """
+    pass
+
+
+def task8():
+    """
+    Given a sequence of words separated by whitespace as input, function prints the words composed of digits only.
+    E.g. "I am not 4 years old. I was born in 1993" -> "4" "1993"
+
+    Input: string
+    Output: None
+    """
+
+
+def task9():
+    """
+    Given a single ASCII code, print and return character as string
+
+    Input: number
+    Output: string
+    """
+
+
+def task10():
+    """
+    Given a many ASCII codes as string separated by space (e.g. "76 78"), print and return characters as string ("LN")
+
+    Input: string
+    Output: string
+    """
+
+
+def task11():
+    """
+    Given an email adress: username@companyname.com" format, function returns the company name
+    of a given email address. (e.g. "gregory@sda.pl -> "sda")
+    You might want to use function match() from regex library, but it's doable without it.
+
+    Input: string
+    Output: string
+    """
+    pass
+
+
+def task12():
     """
     Function that take 3 comma separated 4 digit binary numbers and then check whether they
-    are divisible by 5 or not. The numbers that are divisible by 5 are to be printed and 0 or 1 should be returned
+    are divisible by 5 or not. The numbers that are divisible by 5 are to be printed as 1, if not 0 should be returned
+
     Input: string with three 3 digit binary numbers (e.g. "1111,1110,1100")
-    Output: string with 1 and 0 (e.g. 1,0,0 - because 1111 if 15 and it is divided by 5
+    Output: string with 1 and 0 (e.g. 1,0,0 - because 1111 if 15 and it is divided by 5)
     """
 
 
-def task():
+def task14():
+    """
+    Function that finds the string similarity between two given strings. Here string similarity is defined as follows:
+    1. Find shorter string
+    2. Iterating through shrorter string check if there is such a letter in longer string (ignore case with multiple 
+    same letter in one string)
+    3. Ratio between number found duplicates and length of longer string is ration
+    Print it as percent
+    E.g. "mouse use" -> 60%, 3 letter ("use") / 5 (length of longer string)
+    Later you might improve it to work with duplicates
+
+    Input: two strings
+    Ouput: string with percentage
+    """
+    pass
+
+
+def task15():
+    """
+    Function that accepts simple equations such as: "12+4", "10-5" and print and return the result as float
+
+    Input: one string
+    Ouput: float number
+    """
+    pass
+
+
+def task16():
     """
     Function that checks the validity of password input by users.
     Following are the criteria for checking the password:
@@ -28,20 +164,8 @@ def task():
     Input: string with password
     Output: boolean if it meets the requirements
     """
-
-
-def task():
-    """
-    function that can accept two strings as input and print the string with maximum length in console. If two strings
-    have the same length, then the function should print al l strings line by line.
-
-    """
+    pass
 
 
 if __name__ == '__main__':
     pass
-
-
-
-
-# TODO requires if elif else
